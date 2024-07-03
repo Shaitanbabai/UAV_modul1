5e05b28e929b775007ff894be12cef2280e24739
Shaitanbabai <rebootleo0522@gmail.com>
2024-07-03 09:35:08 +0300

    Update practice13.py

--- practice13.py
+++ practice13.py
@@ -1,5 +1,15 @@
-with open("bpla_storage_info.csv", "r", encoding="utf-8") as file:
+# with open("bpla_storage_info.csv", "r", encoding="utf-8") as file:
+#     title = file.readline()
+#     content = file.readlines()
+# print(title)
+# print(content)
+
+with open("bpla_storage_warehouse.csv", "r", encoding="utf-8") as file:
     title = file.readline()
-    content = file.readlines()
-print(title)
+    content = file.read()
+    content = content.split("\n")
+
+for i in range(len(content)):
+    content[i] = content[i].split(",")
+
 print(content)
